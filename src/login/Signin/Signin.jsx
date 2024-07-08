ae6d6f6dde6d401217d774c82b26625c8a811fc2
Aishwarya Nikam <100349291+nikamaish@users.noreply.github.com>
2024-07-09 00:16:58 +0530

    backend link

--- src/login/Signin/Signin.jsx
+++ src/login/Signin/Signin.jsx
@@ -20,7 +20,7 @@ const Signin = () =>{
         e.preventDefault();
     
         try {
-            const response = await fetch('http://127.0.0.1:5000/login', {
+            const response = await fetch('https://vm-backend-1.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
